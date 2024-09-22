9850d71f43f9a7c6334fe7db435ceeb2ea7162e8
AndreiMoraru123 <andreimoraru1999123@gmail.com>
2024-09-22 19:15:08 +0300

    lint cxx avl

--- avl/cxx/avl.hxx
+++ avl/cxx/avl.hxx
@@ -7,12 +7,10 @@
 using namespace boost::intrusive;
 
 class AVLNode : public avl_set_base_hook<> {
- public:
-  std::uint32_t depth;
-  std::uint32_t count;
-  AVLNode *parent;
-
-  AVLNode() : depth(1), count(1), parent(nullptr) {}
+public:
+  std::uint32_t depth{};
+  std::uint32_t count{};
+  AVLNode *parent{};
 
   bool operator<(const AVLNode &other) const {
     return this->depth < other.depth;
@@ -20,17 +18,19 @@ class AVLNode : public avl_set_base_hook<> {
 };
 
 class RootComp {
- public:
-  bool operator()(const AVLNode &a, const AVLNode &b) const { return false; }
-  bool operator()(const AVLNode &a, const int b) const { return false; }
-  bool operator()(const int a, const AVLNode &b) const { return false; }
+public:
+  auto operator()(const AVLNode &a, const AVLNode &b) const -> bool {
+    return false;
+  }
+  auto operator()(const AVLNode &a, const int b) const -> bool { return false; }
+  auto operator()(const int a, const AVLNode &b) const -> bool { return false; }
 };
 
 class AVLTree {
- private:
+private:
   using AVLSet = avl_set<AVLNode, constant_time_size<true>>;
 
- public:
+public:
   AVLSet tree;
   void fix(AVLNode *node);
   auto root() -> AVLNode *;
